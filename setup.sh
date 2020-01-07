73d2702b5b8c800c8a78c727b05f021532296fb6
Anzo <antonr@live.dk>
2020-01-07 15:27:14 +0100

    Fixed an issue with finding g++

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if ! type "$g++" > /dev/null; then
+if ! type g++ &> /dev/null; then
   echo "g++ was not found - Please install it, and run this program again after."
 else
-g++ -std=c++17 projectBuilder.cpp -o build.o
+	g++ -std=c++17 projectBuilder.cpp -o build.o
 
 echo "Compiling ended - run ./build.o to build your project (after the build.o has been moved to the root of your project)"
 
