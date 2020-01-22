96625b755098860acb4e0badc8d27470959e5905
Anzo <antonr@live.dk>
2020-01-22 22:00:03 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 if ! type g++ &> /dev/null; then
   echo "g++ was not found - Please install it, and then run this program again."
 else
-	g++ -std=c++17 projectBuilder.cpp -o build.o
+	g++ -lstdc++fs -std=c++17 projectBuilder.cpp -o build.o
 
 echo "Compiling ended - run ./build.o to build your project (after the build.o has been moved to the root of your project)"
 
