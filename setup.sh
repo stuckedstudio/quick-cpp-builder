1a2b9db8c151374a28f70bd43c0f7c428f383ec0
Anzo <antonr@live.dk>
2020-01-31 22:15:06 +0100

    changed to clang++

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if ! type g++ &> /dev/null; then
-  echo "g++ was not found - Please install it, and then run this program again."
+if ! type clang++ &> /dev/null; then
+  echo "clang++ was not found - Please install it, and then run this program again."
 else
-	g++ -std=c++17 projectBuilder.cpp -o build.o -lstdc++fs
+	clang++ -std=c++17 projectBuilder.cpp -o build.o -lc++abi -lstdc++fs
 
 echo "Compiling ended - run ./build.o to build your project (after the build.o has been moved to the root of your project)"
 
